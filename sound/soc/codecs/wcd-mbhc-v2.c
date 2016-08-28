3492ed5ffa082f7fea5ebe5af8d1998b77386cb0
TheStrix <parthbhatia98@gmail.com>
2016-08-28 20:57:42 +0530

    sound: soc: codecs: Fix build
    * Modify for CAF's mbhc_micbias_control()
    
    Change-Id: I48558191803fd6832f42cdcdfe8805250e93e478
    Signed-off-by: TheStrix <parthbhatia98@gmail.com>

--- sound/soc/codecs/wcd-mbhc-v2.c
+++ sound/soc/codecs/wcd-mbhc-v2.c
@@ -819,6 +819,7 @@ static void wcd_mbhc_find_plug_and_report(struct wcd_mbhc *mbhc,
 			WCD_MBHC_REG_UPDATE_BITS(WCD_MBHC_FSM_EN, 0);
 			WCD_MBHC_REG_UPDATE_BITS(WCD_MBHC_BTN_ISRC_CTL, 0);
 			mbhc->mbhc_cb->mbhc_micbias_control(mbhc->codec,
+				mbhc->mbhc_cfg->anc_micbias,
 				MICB_PULLUP_DISABLE);
 			/* Setup for insertion detection */
 			WCD_MBHC_REG_UPDATE_BITS(WCD_MBHC_ELECT_DETECTION_TYPE,
@@ -991,6 +992,7 @@ static void wcd_mbhc_update_fsm_source(struct wcd_mbhc *mbhc,
 	case MBHC_PLUG_TYPE_ANC_HEADPHONE:
 		WCD_MBHC_REG_UPDATE_BITS(WCD_MBHC_BTN_ISRC_CTL, 0);
 		mbhc->mbhc_cb->mbhc_micbias_control(mbhc->codec,
+			mbhc->mbhc_cfg->anc_micbias,
 			MICB_PULLUP_ENABLE);
 		break;
 	default:
@@ -1487,6 +1489,7 @@ static void wcd_mbhc_swch_irq_handler(struct wcd_mbhc *mbhc)
 		WCD_MBHC_REG_UPDATE_BITS(WCD_MBHC_FSM_EN, 0);
 		WCD_MBHC_REG_UPDATE_BITS(WCD_MBHC_BTN_ISRC_CTL, 0);
 		mbhc->mbhc_cb->mbhc_micbias_control(mbhc->codec,
+			mbhc->mbhc_cfg->anc_micbias,
 			MICB_PULLUP_DISABLE);
 		if (mbhc->mbhc_cb->mbhc_common_micb_ctrl)
 			mbhc->mbhc_cb->mbhc_common_micb_ctrl(codec,
