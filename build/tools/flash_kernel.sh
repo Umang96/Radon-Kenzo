1e04d8619d0505706c9f3650b1ab688c64456a13
umang96 <umangleekha3@gmail.com>
2016-09-16 00:22:53 +0530

    Radon build tools

---
+++ build/tools/flash_kernel.sh
@@ -0,0 +1,22 @@
+#!/sbin/sh
+ #
+ # Copyright © 2014, Varun Chitre "varun.chitre15" <varun.chitre15@gmail.com> 
+ #
+ # Live ramdisk patching script
+ #
+ # This software is licensed under the terms of the GNU General Public
+ # License version 2, as published by the Free Software Foundation, and
+ # may be copied, distributed, and modified under those terms.
+ #
+ # This program is distributed in the hope that it will be useful,
+ # but WITHOUT ANY WARRANTY; without even the implied warranty of
+ # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ # GNU General Public License for more details.
+ #
+ # Please maintain this if you use this script or any part of it
+ #
+cd /tmp/
+/sbin/busybox dd if=/dev/block/bootdevice/by-name/boot of=./boot.img
+./unpackbootimg -i /tmp/boot.img
+./mkbootimg --kernel /tmp/Image --ramdisk /tmp/boot.img-ramdisk.gz --cmdline "console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 earlyprintk"  --base 0x80000000 --pagesize 2048 --ramdisk_offset 0x02000000 --tags_offset 0x01e00000 --dt /tmp/dt.img -o /tmp/newboot.img
+/sbin/busybox dd if=/tmp/newboot.img of=/dev/block/bootdevice/by-name/boot
---
+++ radon.sh
@@ -0,0 +1,66 @@
+#
+# Custom build script for Radon kernel
+#
+# Copyright 2016 Umang Leekha
+#
+# This software is licensed under the terms of the GNU General Public
+# License version 2, as published by the Free Software Foundation, and
+# may be copied, distributed, and modified under those terms.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# Please maintain this if you use this script or any part of it
+#
+
+yellow='\033[0;33m'
+white='\033[0m'
+red='\033[0;31m'
+gre='\e[0;32m'
+echo -e ""
+echo -e "$yellow ====================================\n Welcome to Radon building program !\n ====================================\n 1.Build Radon stable version\n 2.Build Radon test version"
+echo -n " Enter your choice:"
+read choice
+echo -e "$white"
+Start=$(date +"%s")
+KERNEL_DIR=$PWD
+DTBTOOL=$KERNEL_DIR/dtbTool
+cd $KERNEL_DIR
+export ARCH=arm64
+export CROSS_COMPILE="/home/umang/toolchain/aarch64-linux-linaro-android-4.9/bin/aarch64-linux-android-"
+export LD_LIBRARY_PATH=home/umang/toolchain/aarch64-linux-linaro-android-4.9/lib/
+STRIP="/home/umang/toolchain/aarch64-linux-linaro-android-4.9/bin/aarch64-linux-android-strip"
+make clean
+make cyanogenmod_kenzo_defconfig
+export KBUILD_BUILD_HOST="G5070"
+export KBUILD_BUILD_USER="Umang"
+make -j4
+time=$(date +"%d-%m-%y-%T")
+$DTBTOOL -2 -o $KERNEL_DIR/arch/arm64/boot/dt.img -s 2048 -p $KERNEL_DIR/scripts/dtc/ $KERNEL_DIR/arch/arm/boot/dts/
+mv $KERNEL_DIR/arch/arm64/boot/dt.img $KERNEL_DIR/build/tools/dt.img
+cp $KERNEL_DIR/arch/arm64/boot/Image $KERNEL_DIR/build/tools/Image
+rm -rf $KEREL_DIR/build/system
+mkdir -p $KERNEL_DIR/build/system/lib/modules
+cp $KERNEL_DIR/drivers/staging/prima/wlan.ko $KERNEL_DIR/build/system/lib/modules/wlan.ko
+cd $KERNEL_DIR/build
+rm *.zip
+cd $KERNEL_DIR/build/system/lib/modules/
+$STRIP --strip-unneeded *.ko
+zimage=$KERNEL_DIR/arch/arm64/boot/Image
+if ! [ -a $zimage ];
+then
+echo -e "$red << Failed to compile zImage, fix the errors first >>$white"
+else
+cd $KERNEL_DIR/build
+if [ $choice == 2 ]; then
+zip -r Radon-Kenzo-Test-$time-Cm.zip *
+else
+zip -r Radon-Kenzo-Stable-Cm.zip *
+fi
+End=$(date +"%s")
+Diff=$(($End - $Start))
+echo -e "$gre << Build completed in $(($Diff / 60)) minutes and $(($Diff % 60)) seconds >>$white"
+fi
+cd $KERNEL_DIR
