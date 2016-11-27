a6d77393f382d8b6d47b1b0113913aa7bf0c2f61
umang96 <umangleekha3@gmail.com>
2016-11-27 11:10:49 +0530

    Radon build tools

---
+++ build/tools/edit_ramdisk.sh
@@ -0,0 +1,66 @@
+#!/sbin/sh
+
+CONFIGFILE="/tmp/init.radon.rc"
+echo "on boot" >> $CONFIGFILE
+echo "" >> $CONFIGFILE
+DT2W=$(cat /tmp/aroma/dt2w.prop | cut -d '=' -f2)
+if [ $DT2W == 1 ]; then
+DTP=1
+elif [ $DT2W == 2 ]; then
+DTP=0
+fi 
+echo "write /sys/android_touch/doubletap2wake " $DTP >> $CONFIGFILE
+echo "" >> $CONFIGFILE
+COLOR=$(cat /tmp/aroma/color.prop | cut -d '=' -f2)
+if [ $COLOR == 1 ]; then
+echo "write /sys/devices/platform/kcal_ctrl.0/kcal_sat 270" >> $CONFIGFILE
+echo "write /sys/devices/platform/kcal_ctrl.0/kcal_val 257" >> $CONFIGFILE
+echo "write /sys/devices/platform/kcal_ctrl.0/kcal_cont 265" >> $CONFIGFILE
+elif [ $COLOR == 2 ]; then
+echo "write /sys/devices/platform/kcal_ctrl.0/kcal_sat 255" >> $CONFIGFILE
+echo "write /sys/devices/platform/kcal_ctrl.0/kcal_val 255" >> $CONFIGFILE
+echo "write /sys/devices/platform/kcal_ctrl.0/kcal_cont 255" >> $CONFIGFILE
+fi
+echo "" >> $CONFIGFILE
+INTERACTIVE=$(cat /tmp/aroma/interactive.prop | cut -d '=' -f2)
+
+echo "on enable-low-power" >> $CONFIGFILE
+
+echo "# Disable thermal & BCL core_control to update interactive gov settings" >> $CONFIGFILE
+echo "write /sys/module/msm_thermal/core_control/enabled 0" >> $CONFIGFILE
+echo "write /sys/devices/soc.0/qcom,bcl.56/mode disable" >> $CONFIGFILE
+echo "write /sys/devices/soc.0/qcom,bcl.56/hotplug_mask 0" >> $CONFIGFILE
+echo "write /sys/devices/soc.0/qcom,bcl.56/hotplug_soc_mask 0" >> $CONFIGFILE
+echo "write /sys/devices/soc.0/qcom,bcl.56/mode enable" >> $CONFIGFILE
+echo "" >> $CONFIGFILE
+
+if [ $INTERACTIVE == 1 ]; then
+tld1="\"55 1017600:65 1190400:75 1305600:85 1382400:90 1401600:95"\"
+tld2="\"75 1190400:85 1382400:90 1747200:95"\"
+rate=30000
+elif [ $INTERACTIVE == 2 ]; then
+tld1="\"60 1017600:70 1190400:80 1305600:90 1382400:95 1401600:98"\"
+tld2="\"70 1113600:80 1190400:90 1382400:95 1747200:98"\"
+rate=40000
+elif [ $INTERACTIVE == 3 ]; then
+tld1="\"40 1017600:50 1190400:60 1305600:70 1382400:80 1401600:90"\"
+tld2="\"60 1190400:70 1382400:80 1747200:90"\"
+rate=20000
+fi
+
+echo "write /sys/devices/system/cpu/cpu0/online 1" >> $CONFIGFILE
+echo "write /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor interactive" >> $CONFIGFILE
+echo "write /sys/devices/system/cpu/cpu0/cpufreq/interactive/timer_rate $rate" >> $CONFIGFILE
+echo "write /sys/devices/system/cpu/cpu0/cpufreq/interactive/target_loads $tld1" >> $CONFIGFILE
+echo "write /sys/devices/system/cpu/cpu4/online 1" >> $CONFIGFILE
+echo "write /sys/devices/system/cpu/cpu4/cpufreq/scaling_governor interactive" >> $CONFIGFILE
+echo "write /sys/devices/system/cpu/cpu4/cpufreq/interactive/timer_rate $rate" >> $CONFIGFILE
+echo "write /sys/devices/system/cpu/cpu4/cpufreq/interactive/target_loads $tld2" >>$CONFIGFILE
+echo "" >> $CONFIGFILE
+echo "# Disable thermal & BCL core_control to update interactive gov settings" >> $CONFIGFILE
+echo "write /sys/module/msm_thermal/core_control/enabled 1" >> $CONFIGFILE
+echo "write /sys/devices/soc.0/qcom,bcl.56/mode disable" >> $CONFIGFILE
+echo "write /sys/devices/soc.0/qcom,bcl.56/hotplug_mask 48" >> $CONFIGFILE
+echo "write /sys/devices/soc.0/qcom,bcl.56/hotplug_soc_mask 48" >> $CONFIGFILE
+echo "write /sys/devices/soc.0/qcom,bcl.56/mode enable" >> $CONFIGFILE
+echo "" >> $CONFIGFILE
---
+++ build/tools/flash_kernel.sh
@@ -0,0 +1,61 @@
+#!/sbin/sh
+ #
+ # Copyright © 2014, Varun Chitre "varun.chitre15" <varun.chitre15@gmail.com> 
+ #
+ # Live ramdisk patching script
+ #
+ # This software is licensed under the terms of the GNU General Public
+ # License version 2, as published by the Free Software Foundation, and
+ # may be copied, distributed, and modified under those terms.
+ #
+ # This program is distributed in the hope that it will be useful,
+ # but WITHOUT ANY WARRANTY; without even the implied warranty of
+ # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ # GNU General Public License for more details.
+ #
+ # Please maintain this if you use this script or any part of it
+ #
+goodix=$(cat /tmp/aroma/goodix.prop | cut -d '=' -f2)
+overclock=$(cat /tmp/aroma/overclock.prop | cut -d '=' -f2)
+if ([ $overclock -eq 1 ]&&[ $goodix -eq 1 ]); then
+cmd="console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1"
+dim=/tmp/dt11.img
+zim=/tmp/Image1
+elif ([ $overclock -eq 1 ]&&[ $goodix -eq 2 ]); then
+cmd="console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 androidboot.selinux=permissive"
+dim=/tmp/dt12.img
+zim=/tmp/Image2
+elif ([ $overclock -eq 2 ]&&[ $goodix -eq 1 ]); then
+cmd="console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1"
+dim=/tmp/dt21.img
+zim=/tmp/Image1
+elif ([ $overclock -eq 2 ]&&[ $goodix -eq 2 ]); then
+cmd="console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 androidboot.selinux=permissive"
+dim=/tmp/dt22.img
+zim=/tmp/Image2
+fi 
+cd /tmp/
+/sbin/busybox dd if=/dev/block/bootdevice/by-name/boot of=./boot.img
+./unpackbootimg -i /tmp/boot.img
+mkdir /tmp/ramdisk
+cp /tmp/boot.img-ramdisk.gz /tmp/ramdisk/
+cd /tmp/ramdisk/
+gunzip -c /tmp/ramdisk/boot.img-ramdisk.gz | cpio -i
+rm /tmp/ramdisk/boot.img-ramdisk.gz
+rm /tmp/ramdisk/init.qcom.power.rc
+rm /tmp/boot.img-ramdisk.gz
+cp /tmp/init.radon.rc /tmp/ramdisk/
+cp /tmp/init.qcom.power.rc /tmp/ramdisk/
+chmod 0750 /tmp/ramdisk/init.qcom.power.rc
+chmod 0750 /tmp/ramdisk/init.radon.rc
+if [ $(grep -c "import /init.qcom.power.rc" /tmp/ramdisk/init.rc) == 0 ]; then
+   sed -i "/import \/init\.environ\.rc/aimport /init.qcom.power.rc" /tmp/ramdisk/init.rc
+fi
+if [ $(grep -c "import /init.radon.rc" /tmp/ramdisk/init.rc) == 0 ]; then
+   sed -i "/import \/init\.environ\.rc/aimport /init.radon.rc" /tmp/ramdisk/init.rc
+fi
+find . | cpio -o -H newc | gzip > /tmp/boot.img-ramdisk.gz
+rm -r /tmp/ramdisk
+cd /tmp/
+./mkbootimg --kernel $zim --ramdisk /tmp/boot.img-ramdisk.gz --cmdline "$cmd"  --base 0x80000000 --pagesize 2048 --ramdisk_offset 0x02000000 --tags_offset 0x01e00000 --dt $dim -o /tmp/newboot.img
+/sbin/busybox dd if=/tmp/newboot.img of=/dev/block/bootdevice/by-name/boot
