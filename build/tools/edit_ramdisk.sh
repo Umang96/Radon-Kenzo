11452565cb9826f797c87a047d7fc8757ab1802c
umang96 <umangleekha3@gmail.com>
2016-12-05 23:46:03 +0530

    Update build tools

--- build/tools/edit_ramdisk.sh
+++ build/tools/edit_ramdisk.sh
@@ -16,12 +16,12 @@ if [ $COLOR == 1 ]; then
 echo "write /sys/devices/platform/kcal_ctrl.0/kcal_sat 275" >> $CONFIGFILE
 echo "write /sys/devices/platform/kcal_ctrl.0/kcal_val 249" >> $CONFIGFILE
 echo "write /sys/devices/platform/kcal_ctrl.0/kcal_cont 264" >> $CONFIGFILE
-echo "write /sys/devices/platform/kcal_ctrl.0/kcal 256 250 240" >> $CONFIGFILE
+echo "write /sys/devices/platform/kcal_ctrl.0/kcal \"256 250 240"\" >> $CONFIGFILE
 elif [ $COLOR == 2 ]; then
 echo "write /sys/devices/platform/kcal_ctrl.0/kcal_sat 255" >> $CONFIGFILE
 echo "write /sys/devices/platform/kcal_ctrl.0/kcal_val 255" >> $CONFIGFILE
 echo "write /sys/devices/platform/kcal_ctrl.0/kcal_cont 255" >> $CONFIGFILE
-echo "write /sys/devices/platform/kcal_ctrl.0/kcal 256 256 256" >> $CONFIGFILE
+echo "write /sys/devices/platform/kcal_ctrl.0/kcal \"256 256 256"\" >> $CONFIGFILE
 fi
 echo "" >> $CONFIGFILE
 INTERACTIVE=$(cat /tmp/aroma/interactive.prop | cut -d '=' -f2)
@@ -35,20 +35,6 @@ echo "write /sys/devices/soc.0/qcom,bcl.56/hotplug_mask 0" >> $CONFIGFILE
 echo "write /sys/devices/soc.0/qcom,bcl.56/hotplug_soc_mask 0" >> $CONFIGFILE
 echo "write /sys/devices/soc.0/qcom,bcl.56/mode enable" >> $CONFIGFILE
 echo "" >> $CONFIGFILE
-
-if [ $INTERACTIVE == 1 ]; then
-tld1="\"55 1017600:65 1190400:75 1305600:80 1382400:90 1401600:95"\"
-tld2="\"75 1190400:85 1382400:90 1747200:95"\"
-rate=30000
-elif [ $INTERACTIVE == 2 ]; then
-tld1="\"60 1017600:70 1190400:80 1305600:90 1382400:95 1401600:98"\"
-tld2="\"70 1113600:80 1190400:90 1382400:95 1747200:98"\"
-rate=40000
-elif [ $INTERACTIVE == 3 ]; then
-tld1="\"40 1017600:50 1190400:60 1305600:70 1382400:80 1401600:90"\"
-tld2="\"60 1190400:70 1382400:80 1747200:90"\"
-rate=20000
-fi
 
 echo "write /sys/devices/system/cpu/cpu0/online 1" >> $CONFIGFILE
 echo "write /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor interactive" >> $CONFIGFILE
