514abbd19b7462d8e581668ef0e96a8da28d6795
umang96 <umangleekha3@gmail.com>
2016-12-20 21:08:06 +0530

    Make selinux mode optional

--- radon.sh
+++ radon.sh
@@ -36,9 +36,9 @@ Start=$(date +"%s")
 DTBTOOL=$KERNEL_DIR/dtbTool
 cd $KERNEL_DIR
 export ARCH=arm64
-export CROSS_COMPILE="/home/$USER/toolchain/aarch64-linux-linaro-android-4.9/bin/aarch64-linux-android-"
-export LD_LIBRARY_PATH=home/$USER/toolchain/aarch64-linux-linaro-android-4.9/lib/
-STRIP="/home/$USER/toolchain/aarch64-linux-linaro-android-4.9/bin/aarch64-linux-android-strip"
+export CROSS_COMPILE="/home/$USER/toolchain/aarch64-linux-google-android-4.9/bin/aarch64-linux-android-"
+export LD_LIBRARY_PATH=home/$USER/toolchain/aarch64-linux-google-android-4.9/lib/
+STRIP="/home/$USER/toolchain/aarch64-linux-google-android-4.9/bin/aarch64-linux-android-strip"
 cp $KERNEL_DIR/build/modules/wlan1.ko ~/wlan1.ko
 cp $KERNEL_DIR/build/modules/wlan2.ko ~/wlan2.ko
 make clean
