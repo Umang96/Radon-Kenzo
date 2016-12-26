cad7f0f2e1be4839d24a8b2de7a8555f8253462c
umang96 <umangleekha3@gmail.com>
2016-12-26 15:36:21 +0530

    Update build tools

--- build/tools/edit_ramdisk.sh
+++ build/tools/edit_ramdisk.sh
@@ -5,12 +5,18 @@ INTERACTIVE=$(cat /tmp/aroma/interactive.prop | cut -d '=' -f2)
 if [ $INTERACTIVE == 1 ]; then
 TLS="55 1017600:65 1190400:75 1305600:80 1382400:90 1401600:95"
 TLB="75 1190400:85 1382400:90 1747200:95"
+FMS=691200
+FMB=883200
 elif [ $INTERACTIVE == 2 ]; then
 TLS="65 1017600:75 1190400:85 1305600:90 1382400:95 1401600:99"
 TLB="75 1190400:85 1382400:90 1747200:99"
+FMS=400000
+FMB=400000
 elif [ $INTERACTIVE == 3 ]; then
 TLS="45 1017600:55 1190400:65 1305600:75 1382400:80 1401600:90"
 TLB="65 1190400:75 1382400:80 1747200:95"
+FMS=691200
+FMB=883200
 fi
 echo "on boot" >> $CONFIGFILE
 echo "" >> $CONFIGFILE
@@ -42,31 +48,51 @@ echo "write /sys/devices/platform/kcal_ctrl.0/kcal_val 255" >> $CONFIGFILE
 echo "write /sys/devices/platform/kcal_ctrl.0/kcal_cont 255" >> $CONFIGFILE
 echo "write /sys/devices/platform/kcal_ctrl.0/kcal \"256 256 256"\" >> $CONFIGFILE
 fi
-#echo "" >> $CONFIGFILE
-#echo "# CHARGING RATE" >> $CONFIGFILE
-#CRATE=$(cat /tmp/aroma/crate.prop | cut -d '=' -f2)
-#if [ $CRATE == 5 ]; then
-#CHG=2400
-#elif [ $CRATE == 4 ]; then
-#CHG=2200
-#elif [ $CRATE == 3 ]; then
-#CHG=2000
-#elif [ $CRATE == 2 ]; then
-#CHG=1800
-#elif [ $CRATE == 1 ]; then
-#CHG=1600
-#fi 
-#echo "write /sys/module/qpnp_smbcharger/parameters/default_dcp_icl_ma $CHG" >> $CONFIGFILE
-#echo "write /sys/module/qpnp_smbcharger/parameters/default_hvdcp_icl_ma $CHG" >> $CONFIGFILE
+echo "" >> $CONFIGFILE
+echo "# CHARGING RATE" >> $CONFIGFILE
+CRATE=$(cat /tmp/aroma/crate.prop | cut -d '=' -f2)
+if [ $CRATE == 5 ]; then
+CHG=2400
+elif [ $CRATE == 4 ]; then
+CHG=2200
+elif [ $CRATE == 3 ]; then
+CHG=2000
+elif [ $CRATE == 2 ]; then
+CHG=1800
+elif [ $CRATE == 1 ]; then
+CHG=1600
+fi 
+echo "chmod 666 /sys/module/qpnp_smbcharger/parameters/default_dcp_icl_ma" >> $CONFIGFILE
+echo "chmod 666 /sys/module/qpnp_smbcharger/parameters/default_hvdcp_icl_ma" >> $CONFIGFILE
+echo "write /sys/module/qpnp_smbcharger/parameters/default_dcp_icl_ma $CHG" >> $CONFIGFILE
+echo "write /sys/module/qpnp_smbcharger/parameters/default_hvdcp_icl_ma $CHG" >> $CONFIGFILE
 HOTPLUG=$(cat /tmp/aroma/hotplug.prop | cut -d '=' -f2)
+echo "" >> $CONFIGFILE
+echo "on enable-low-power" >> $CONFIGFILE
+echo "" >> $CONFIGFILE
+echo "# DISABLE BCL & CORE CTL" >> $CONFIGFILE
+echo "write /sys/module/msm_thermal/core_control/enabled 0" >> $CONFIGFILE
+echo "write /sys/devices/soc.0/qcom,bcl.56/mode disable" >> $CONFIGFILE
+echo "write /sys/devices/soc.0/qcom,bcl.56/hotplug_mask 0" >> $CONFIGFILE
+echo "write /sys/devices/soc.0/qcom,bcl.56/hotplug_soc_mask 0" >> $CONFIGFILE
+echo "write /sys/devices/soc.0/qcom,bcl.56/mode disable" >> $CONFIGFILE
+echo "" >> $CONFIGFILE
+echo "# BRING CORES ONLINE" >> $CONFIGFILE
+echo "write /sys/devices/system/cpu/cpu0/online 1" >> $CONFIGFILE
+echo "write /sys/devices/system/cpu/cpu1/online 1" >> $CONFIGFILE
+echo "write /sys/devices/system/cpu/cpu2/online 1" >> $CONFIGFILE
+echo "write /sys/devices/system/cpu/cpu3/online 1" >> $CONFIGFILE
+echo "write /sys/devices/system/cpu/cpu4/online 1" >> $CONFIGFILE
+echo "write /sys/devices/system/cpu/cpu5/online 1" >> $CONFIGFILE
+echo "" >> $CONFIGFILE
 echo "# HOTPLUGGING" >> $CONFIGFILE
 if [ $HOTPLUG == 1 ]; then
 echo "write /sys/devices/system/cpu/cpu4/core_ctl/not_preferred \"1 0\"" >> $CONFIGFILE
 echo "write /sys/devices/system/cpu/cpu4/core_ctl/min_cpus 1" >> $CONFIGFILE
 echo "write /sys/devices/system/cpu/cpu4/core_ctl/max_cpus 2" >> $CONFIGFILE
-echo "write /sys/devices/system/cpu/cpu4/core_ctl/busy_up_thres 30" >> $CONFIGFILE
-echo "write /sys/devices/system/cpu/cpu4/core_ctl/busy_down_thres 25" >> $CONFIGFILE
-echo "write /sys/devices/system/cpu/cpu4/core_ctl/offline_delay_ms 1600" >> $CONFIGFILE
+echo "write /sys/devices/system/cpu/cpu4/core_ctl/busy_up_thres 35" >> $CONFIGFILE
+echo "write /sys/devices/system/cpu/cpu4/core_ctl/busy_down_thres 30" >> $CONFIGFILE
+echo "write /sys/devices/system/cpu/cpu4/core_ctl/offline_delay_ms 1000" >> $CONFIGFILE
 echo "write /sys/devices/system/cpu/cpu4/core_ctl/task_thres 4" >> $CONFIGFILE
 echo "write /sys/devices/system/cpu/cpu4/core_ctl/is_big_cluster 1" >> $CONFIGFILE
 echo "write /sys/devices/system/cpu/cpu0/core_ctl/not_preferred \"1 0 0 0\"" >> $CONFIGFILE
@@ -93,29 +119,23 @@ echo "write /sys/devices/system/cpu/cpu0/core_ctl/is_big_cluster 0" >> $CONFIGFI
 fi
 echo "chmod 0444 /sys/devices/system/cpu/cpu4/core_ctl/min_cpus" >> $CONFIGFILE
 echo "" >> $CONFIGFILE
-echo "on enable-low-power" >> $CONFIGFILE
-echo "" >> $CONFIGFILE
 echo "# DISABLE BCL & CORE CTL" >> $CONFIGFILE
-echo "write /sys/module/msm_thermal/core_control/enabled 0" >> $CONFIGFILE
+echo "write /sys/module/msm_thermal/core_control/enabled 1" >> $CONFIGFILE
 echo "write /sys/devices/soc.0/qcom,bcl.56/mode disable" >> $CONFIGFILE
 echo "write /sys/devices/soc.0/qcom,bcl.56/hotplug_mask 0" >> $CONFIGFILE
 echo "write /sys/devices/soc.0/qcom,bcl.56/hotplug_soc_mask 0" >> $CONFIGFILE
 echo "write /sys/devices/soc.0/qcom,bcl.56/mode disable" >> $CONFIGFILE
 echo "" >> $CONFIGFILE
-echo "# BRING CORES ONLINE" >> $CONFIGFILE
-echo "write /sys/devices/system/cpu/cpu0/online 1" >> $CONFIGFILE
-echo "write /sys/devices/system/cpu/cpu1/online 1" >> $CONFIGFILE
-echo "write /sys/devices/system/cpu/cpu2/online 1" >> $CONFIGFILE
-echo "write /sys/devices/system/cpu/cpu3/online 1" >> $CONFIGFILE
-echo "write /sys/devices/system/cpu/cpu4/online 1" >> $CONFIGFILE
-echo "write /sys/devices/system/cpu/cpu5/online 1" >> $CONFIGFILE
-echo "" >> $CONFIGFILE
-echo "# ENABLE A53 CLUSTER GOVERNOR" >> $CONFIGFILE
+echo "# TWEAK A53 CLUSTER GOVERNOR" >> $CONFIGFILE
 echo "write /sys/devices/system/cpu/cpu0/online 1" >> $CONFIGFILE
 echo "write /sys/devices/system/cpu/cpu0/cpufreq/interactive/target_loads \"$TLS\"" >> $CONFIGFILE
-echo "write /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq 400000" >> $CONFIGFILE
+echo "write /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq $FMS" >> $CONFIGFILE
 echo "" >> $CONFIGFILE
-echo "# ENABLE A72 CLUSTER GOVERNOR" >> $CONFIGFILE
+echo "# TWEAK A72 CLUSTER GOVERNOR" >> $CONFIGFILE
 echo "write /sys/devices/system/cpu/cpu4/online 1" >> $CONFIGFILE
 echo "write /sys/devices/system/cpu/cpu4/cpufreq/interactive/target_loads \"$TLB\"" >> $CONFIGFILE
-echo "write /sys/devices/system/cpu/cpu4/cpufreq/scaling_min_freq 400000" >> $CONFIGFILE
+echo "write /sys/devices/system/cpu/cpu4/cpufreq/scaling_min_freq $FMB" >> $CONFIGFILE
+echo "" >> $CONFIGFILE
+echo "on property:dev.bootcomplete=1" >> $CONFIGFILE
+echo "# SET IO SCHEDULER" >> $CONFIGFILE
+echo "setprop sys.io.scheduler \"fiops\"" >> $CONFIGFILE
