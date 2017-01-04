4b4ef50386ab079c1461554b50fe639d3e8c1a94
umang96 <umangleekha3@gmail.com>
2017-01-04 20:07:55 +0530

    Update build tools

--- build/tools/edit_ramdisk.sh
+++ build/tools/edit_ramdisk.sh
@@ -7,16 +7,31 @@ TLS="55 1017600:65 1190400:75 1305600:80 1382400:90 1401600:95"
 TLB="75 1190400:85 1382400:90 1747200:95"
 FMS=691200
 FMB=883200
+TR=20000
+UTS=30
+DTS=25
+UTB=35
+DTB=30
 elif [ $INTERACTIVE == 2 ]; then
 TLS="65 1017600:75 1190400:85 1305600:90 1382400:95 1401600:99"
 TLB="75 1190400:85 1382400:90 1747200:99"
 FMS=400000
 FMB=400000
+TR=30000
+UTS=35
+DTS=30
+UTB=45
+DTB=40
 elif [ $INTERACTIVE == 3 ]; then
 TLS="45 1017600:55 1190400:65 1305600:75 1382400:80 1401600:90"
 TLB="65 1190400:75 1382400:80 1747200:95"
 FMS=691200
 FMB=883200
+TR=20000
+UTS=25
+DTS=20
+UTB=35
+DTB=30
 fi
 echo "on boot" >> $CONFIGFILE
 echo "" >> $CONFIGFILE
@@ -93,16 +108,16 @@ if [ $HOTPLUG == 1 ]; then
 echo "write /sys/devices/system/cpu/cpu4/core_ctl/not_preferred \"1 0\"" >> $CONFIGFILE
 echo "write /sys/devices/system/cpu/cpu4/core_ctl/min_cpus 1" >> $CONFIGFILE
 echo "write /sys/devices/system/cpu/cpu4/core_ctl/max_cpus 2" >> $CONFIGFILE
-echo "write /sys/devices/system/cpu/cpu4/core_ctl/busy_up_thres 35" >> $CONFIGFILE
-echo "write /sys/devices/system/cpu/cpu4/core_ctl/busy_down_thres 30" >> $CONFIGFILE
+echo "write /sys/devices/system/cpu/cpu4/core_ctl/busy_up_thres $UTB" >> $CONFIGFILE
+echo "write /sys/devices/system/cpu/cpu4/core_ctl/busy_down_thres $DTB" >> $CONFIGFILE
 echo "write /sys/devices/system/cpu/cpu4/core_ctl/offline_delay_ms 1000" >> $CONFIGFILE
 echo "write /sys/devices/system/cpu/cpu4/core_ctl/task_thres 4" >> $CONFIGFILE
 echo "write /sys/devices/system/cpu/cpu4/core_ctl/is_big_cluster 1" >> $CONFIGFILE
 echo "write /sys/devices/system/cpu/cpu0/core_ctl/not_preferred \"1 0 0 0\"" >> $CONFIGFILE
 echo "write /sys/devices/system/cpu/cpu0/core_ctl/min_cpus 2" >> $CONFIGFILE
 echo "write /sys/devices/system/cpu/cpu0/core_ctl/max_cpus 4" >> $CONFIGFILE
-echo "write /sys/devices/system/cpu/cpu0/core_ctl/busy_up_thres 25" >> $CONFIGFILE
-echo "write /sys/devices/system/cpu/cpu0/core_ctl/busy_down_thres 20" >> $CONFIGFILE
+echo "write /sys/devices/system/cpu/cpu0/core_ctl/busy_up_thres $UTS" >> $CONFIGFILE
+echo "write /sys/devices/system/cpu/cpu0/core_ctl/busy_down_thres $DTS" >> $CONFIGFILE
 echo "write /sys/devices/system/cpu/cpu0/core_ctl/offline_delay_ms 1800" >> $CONFIGFILE
 echo "write /sys/devices/system/cpu/cpu0/core_ctl/is_big_cluster 0" >> $CONFIGFILE
 elif [ $HOTPLUG == 2 ]; then
@@ -131,11 +146,13 @@ echo "write /sys/devices/soc.0/qcom,bcl.56/mode disable" >> $CONFIGFILE
 echo "" >> $CONFIGFILE
 echo "# TWEAK A53 CLUSTER GOVERNOR" >> $CONFIGFILE
 echo "write /sys/devices/system/cpu/cpu0/online 1" >> $CONFIGFILE
+echo "write /sys/devices/system/cpu/cpu0/cpufreq/interactive/timer_rate $TR" >> $CONFIGFILE
 echo "write /sys/devices/system/cpu/cpu0/cpufreq/interactive/target_loads \"$TLS\"" >> $CONFIGFILE
 echo "write /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq $FMS" >> $CONFIGFILE
 echo "" >> $CONFIGFILE
 echo "# TWEAK A72 CLUSTER GOVERNOR" >> $CONFIGFILE
 echo "write /sys/devices/system/cpu/cpu4/online 1" >> $CONFIGFILE
+echo "write /sys/devices/system/cpu/cpu4/cpufreq/interactive/timer_rate $TR" >> $CONFIGFILE
 echo "write /sys/devices/system/cpu/cpu4/cpufreq/interactive/target_loads \"$TLB\"" >> $CONFIGFILE
 echo "write /sys/devices/system/cpu/cpu4/cpufreq/scaling_min_freq $FMB" >> $CONFIGFILE
 echo "" >> $CONFIGFILE
