5e804578b164bbdaaa3ae0706fce749d985d80a1
umang96 <umangleekha3@gmail.com>
2017-01-14 18:51:39 +0530

    Update build tools

--- radon.sh
+++ radon.sh
@@ -20,16 +20,12 @@ white='\033[0m'
 red='\033[0;31m'
 gre='\e[0;32m'
 echo -e ""
-echo -e "$gre ====================================\n\n Welcome to Radon building program !\n\n ====================================\n\n 1.Build Radon kenzo stock\n\n 2.Build Radon kenzo overclock\n"
-echo -n " Enter your choice:"
-read overclock
-echo -e "\n 1.Build radon miui mm fpc\n\n 2.Build radon miui mm gdx\n"
+echo -e "$gre ====================================\n\n Welcome to Radon building program !\n\n ====================================\n\n 1.Build radon miui mm fpc\n\n 2.Build radon miui mm gdx\n"
 echo -n " Enter your choice:"
 read goodix
 echo -e "$white"
 KERNEL_DIR=$PWD
 cd arch/arm/boot/dts/
-rm .msm8956*
 rm *.dtb
 cd $KERNEL_DIR
 Start=$(date +"%s")
@@ -44,11 +40,6 @@ cp $KERNEL_DIR/build/modules/wlan2.ko ~/wlan2.ko
 make clean
 mv ~/wlan1.ko $KERNEL_DIR/build/modules/wlan1.ko
 mv ~/wlan2.ko $KERNEL_DIR/build/modules/wlan2.ko
-if [ $overclock == 2 ]; then
-git apply oc.patch
-elif [ $overclock == 1 ]; then
-git apply -R oc.patch
-fi
 if [ $goodix == 2 ]; then
 git apply goodix.patch
 elif [ $goodix == 1 ]; then
@@ -60,14 +51,10 @@ export KBUILD_BUILD_USER="umang"
 make -j4
 time=$(date +"%d-%m-%y-%T")
 $DTBTOOL -2 -o $KERNEL_DIR/arch/arm64/boot/dt.img -s 2048 -p $KERNEL_DIR/scripts/dtc/ $KERNEL_DIR/arch/arm/boot/dts/
-if ([ $overclock -eq 1 ]&&[ $goodix -eq 1 ]); then
-mv $KERNEL_DIR/arch/arm64/boot/dt.img $KERNEL_DIR/build/tools/dt11.img
-elif ([ $overclock -eq 2 ]&&[ $goodix -eq 1 ]); then
-mv $KERNEL_DIR/arch/arm64/boot/dt.img $KERNEL_DIR/build/tools/dt21.img
-elif ([ $overclock -eq 1 ]&&[ $goodix -eq 2 ]); then
-mv $KERNEL_DIR/arch/arm64/boot/dt.img $KERNEL_DIR/build/tools/dt12.img
-elif ([ $overclock -eq 2 ]&&[ $goodix -eq 2 ]); then
-mv $KERNEL_DIR/arch/arm64/boot/dt.img $KERNEL_DIR/build/tools/dt22.img
+if [ $goodix -eq 1 ]; then
+mv $KERNEL_DIR/arch/arm64/boot/dt.img $KERNEL_DIR/build/tools/dt1.img
+elif [ $goodix -eq 2 ]; then
+mv $KERNEL_DIR/arch/arm64/boot/dt.img $KERNEL_DIR/build/tools/dt2.img
 fi
 if [ $goodix == 1 ]; then
 cp $KERNEL_DIR/arch/arm64/boot/Image $KERNEL_DIR/build/tools/Image1
@@ -88,12 +75,9 @@ rm *.zip
 zip -r Radon-Kenzo-Mi-Mm.zip *
 End=$(date +"%s")
 Diff=$(($End - $Start))
-echo -e "$gre << Build completed in $(($Diff / 60)) minutes and $(($Diff % 60)) seconds, variant($overclock$goodix) >>$white"
+echo -e "$gre << Build completed in $(($Diff / 60)) minutes and $(($Diff % 60)) seconds, variant($goodix) >>$white"
 fi
 cd $KERNEL_DIR
-if [ $overclock == 2 ]; then
-git apply -R oc.patch
-fi
 if [ $goodix == 2 ]; then
 git apply -R goodix.patch
 fi
