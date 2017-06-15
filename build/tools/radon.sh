eed829014e5962645b4548c61ca61d2d1ccbab0f
umang96 <umangleekha3@gmail.com>
2017-06-15 23:39:14 +0530

    Update build tools

--- build/tools/radon.sh
+++ build/tools/radon.sh
@@ -8,4 +8,25 @@
  #
  # Enjoy !
  #
+    MemTotalStr=`cat /proc/meminfo | grep MemTotal`
+    MemTotal=${MemTotalStr:16:8}
+    #Set Low memory killer minfree parameters
+    # 64 bit up to 2GB with use 14K, and above 2GB will use 18K
+    #
+    # Set ALMK parameters (usually above the highest minfree values)
+    # 64 bit will have 81K 
+    chmod 0660 /sys/module/lowmemorykiller/parameters/minfree
 
+    if [ $MemTotal -gt 2000000 ]; then
+        echo 0 > /sys/module/lowmemorykiller/parameters/enable_adaptive_lmk
+        echo 10 > /sys/module/process_reclaim/parameters/pressure_min
+        echo 1024 > /sys/module/process_reclaim/parameters/per_swap_size
+        echo "18432,23040,27648,32256,55296,80640" > /sys/module/lowmemorykiller/parameters/minfree
+        echo 81250 > /sys/module/lowmemorykiller/parameters/vmpressure_file_min
+    else
+        echo 1 > /sys/module/lowmemorykiller/parameters/enable_adaptive_lmk
+        echo 10 > /sys/module/process_reclaim/parameters/pressure_min
+        echo 1024 > /sys/module/process_reclaim/parameters/per_swap_size
+        echo "14746,18432,22118,25805,40000,55000" > /sys/module/lowmemorykiller/parameters/minfree
+        echo 81250 > /sys/module/lowmemorykiller/parameters/vmpressure_file_min 
+    fi
